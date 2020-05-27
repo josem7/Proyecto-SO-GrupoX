38b216f6140c52311291a2fdd87d53d883162b9a
josem7 <jndominguez@uc.cl>
2020-05-27 01:54:46 -0400

    cr_open Done

--- src/crfs/cr_API.h
+++ src/crfs/cr_API.h
@@ -1,7 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include <errno.h>
 #include <stdbool.h>
-#include <unistd.h>
-#include <stdio.h>
+#include <inttypes.h>
+#include <stdint.h>
 #pragma once
 
 
@@ -10,16 +14,25 @@ int PARTITION_SIZE;
 int BLOCK_SIZE;
 char* MOUNTED_DISK;
 
+//Estructuras
+typedef struct crFILE {
+  char *name;
+  unsigned int indexLocation;
+  uint64_t size;
+  unsigned int references;
+} crFILE;
+//Funciones Utiles
+void delete_crFILE(crFILE *cr_File);
+
+
 // Asigna la variable global MOUNTED_DISK
 void cr_mount(char* diskname);
 
 // Función para imprimir el bitmap
 void cr_bitmap(unsigned disk, bool hex);
 
-<<<<<<< HEAD
 // Crea un hardlink entre orig y dest
 int cr_hardlink(unsigned disk, char* orig, char* dest);
-=======
 // Función para ver si un archivo existe en una determinada partición. Retorna 1
 //si el archivo existe y 0 en caso contrario.
 int cr_exists(unsigned disk, char* filename);
@@ -27,4 +40,7 @@ int cr_exists(unsigned disk, char* filename);
 //Función para listar los elementos de una partición. Imprime en pantalla
 //los nombres de todos los archivos contenidos en la partición indicado por disk
 void cr_ls(unsigned disk);
->>>>>>> 0d974378225a6ed6b69c8cf618ca391485d86cb0
+
+//Función para abrir un archivo
+//y se retorna un nuevo crFILE* que lo representa.
+crFILE* cr_open(unsigned disk, char* filename, char mode);
--- src/crfs/main.c
+++ src/crfs/main.c
@@ -10,7 +10,9 @@ int main (int argc, char* argv[]) {
   char* diskName =  argv[1];
   cr_mount(diskName);
   printf("%s\n",MOUNTED_DISK);
-  cr_ls(4);
-  int result = cr_exists(3, "C.mkv");
+  int result = cr_exists(1, "Baroque.mp3");
   printf("%d\n",result );
+  //cr_directory(1);
+  crFILE *cr_File = cr_open(2, "Hola.txt", 'w');
+  delete_crFILE(cr_File);
 }
