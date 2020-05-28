42eba229d9628aa0cf1bfea35fa2cd680ebbbafd
xariveros <xariveros@uc.cl>
2020-05-28 00:28:16 -0400

    fix algunos warnings de funciones y liberacion de memoria

--- src/crfs/cr_API.h
+++ src/crfs/cr_API.h
@@ -8,14 +8,14 @@
 #include <stdint.h>
 #pragma once
 
-
 // Global Variables
 int PARTITION_SIZE;
 int BLOCK_SIZE;
-char* MOUNTED_DISK;
+char *MOUNTED_DISK;
 
 //Estructuras
-typedef struct crFILE {
+typedef struct crFILE
+{
   char *name;
   unsigned int indexLocation;
   uint64_t size;
@@ -24,19 +24,18 @@ typedef struct crFILE {
 //Funciones Utiles
 void delete_crFILE(crFILE *cr_File);
 
-
 // Asigna la variable global MOUNTED_DISK
-void cr_mount(char* diskname);
+void cr_mount(char *diskname);
 
 // Función para imprimir el bitmap
 void cr_bitmap(unsigned disk, bool hex);
 
 // Crea un hardlink entre orig y dest
-int cr_hardlink(unsigned disk, char* orig, char* dest);
+int cr_hardlink(unsigned disk, char *orig, char *dest);
 
 // Función para ver si un archivo existe en una determinada partición. Retorna 1
 //si el archivo existe y 0 en caso contrario.
-int cr_exists(unsigned disk, char* filename);
+int cr_exists(unsigned disk, char *filename);
 
 //Función para listar los elementos de una partición. Imprime en pantalla
 //los nombres de todos los archivos contenidos en la partición indicado por disk
@@ -44,6 +43,8 @@ void cr_ls(unsigned disk);
 
 void cr_directory(unsigned disk);
 
+int cr_close(crFILE *file_desc);
+
 //Función para abrir un archivo
 //y se retorna un nuevo crFILE* que lo representa.
-crFILE* cr_open(unsigned disk, char* filename, char mode);
+crFILE *cr_open(unsigned disk, char *filename, char mode);
--- src/crfs/main.c
+++ src/crfs/main.c
@@ -6,13 +6,16 @@
 #include <stdbool.h>
 #include "cr_API.h"
 
-int main (int argc, char* argv[]) {
-  char* diskName =  argv[1];
+int main(int argc, char *argv[])
+{
+  char *diskName = argv[1];
   cr_mount(diskName);
-  printf("%s\n",MOUNTED_DISK);
+  printf("%s\n", MOUNTED_DISK);
   cr_ls(1);
   int result = cr_exists(1, "Baroque.mp3");
   printf("%d\n", result);
+  crFILE *file = cr_open(1, "QPC.gif", 'r');
+  cr_close(file);
   cr_hardlink(1, "text.txt", "texto.txt");
   cr_ls(1);
 }
